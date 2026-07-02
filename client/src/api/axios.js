86170cb744a00379dff39ae4e9e7db55f6f6af02
sabarinarayanan8114 <dsabari2408@gmail.com>
2026-07-02 14:16:36 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://expense-tracker-project-vwoo.onrender.com/api",
     headers: {
         "Content-type" : "application/json"
     }
